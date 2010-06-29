eae076a7abe74db0c84e354ea6b93ecffaa3860a
shawn.won <shawn.won@a068ff56-318e-4133-19a1-12da139e03e6>
2010-06-29 07:32:18 +0000

    CLOSED - # 5: Deploy 실패시 로그를 찍고 Error Log 뷰를 보여줌. http://svn.bpnr.co.kr/trac/bpnrcodes/ticket/5

--- com.bpnr.portal.devtools/src/com/bpnr/portal/devtools/actions/DeployProgressRunnable.java
+++ com.bpnr.portal.devtools/src/com/bpnr/portal/devtools/actions/DeployProgressRunnable.java
@@ -10,8 +10,11 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.PartInitException;
 
 import com.bpnr.portal.devtools.EclipseIDE;
+import com.bpnr.portal.devtools.PdkToolsActivator;
 import com.bpnr.portal.devtools.PdkToolsLog;
 import com.bpnr.portal.devtools.ProgressMonitors;
 import com.bpnr.portal.devtools.preferences.PortalServer;
@@ -68,20 +71,30 @@ public class DeployProgressRunnable implements IRunnableWithProgress {
 
 	private void processUpload(IProgressMonitor monitor, File prospectedParFile) {
 		monitor.subTask("Deploying archive...");
+		boolean deploySuccess = false;
 		try {
 			DeployEngine deployEngine = new DeployEngine(server);
 			System.out.println(server);
 			deployEngine.deploy(prospectedParFile);
+			deploySuccess = true;
 		} catch (UnknownHostException e) {
-			PdkToolsLog.logError("Operation failed: Unknown host \nPlease make sure the server '" + server.getName() + "' (" + server.getHost() + ":" + server.getPort() + ") exists and is running.",
+			PdkToolsLog.logError("Operation failed: Unknown host. Please make sure the server '" + server.getName() + "' (" + server.getHost() + ":" + server.getPort() + ") exists and is running.",
 					e);
 		} catch (ConnectException e) {
-			PdkToolsLog.logError("Operation failed: " + e.getMessage() + "\nPlease make sure the server '" + server.getName() + "' (" + server.getHost() + ":" + server.getPort()
+			PdkToolsLog.logError("Operation failed: " + e.getMessage() + ". Please make sure the server '" + server.getName() + "' (" + server.getHost() + ":" + server.getPort()
 					+ ") is running. Unable to connect to the Portal", e);
 		} catch (DeploymentException e) {
 			PdkToolsLog.logError("PAR upload failed: " + e.getTargetComponent() + ". Please check the user ID and password.", e);
 		} catch (Exception e) {
 			PdkToolsLog.logError(e);
+		} finally {
+			if (!deploySuccess) {
+				try {
+					PdkToolsActivator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().showView("org.eclipse.pde.runtime.LogView");
+				} catch (PartInitException e) {
+					PdkToolsLog.logError(e);
+				}
+			}
 		}
 	}
 
